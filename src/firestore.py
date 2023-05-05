900bf57282b1aee2c8b5814c39cfbe3ed4d825be
ceshmina <shu@ceshmina.com>
2023-05-05 22:07:07 +0900

    Use date[8:] as document ID, and update create/update time

--- src/firestore.py
+++ src/firestore.py
@@ -1,4 +1,4 @@
-from google.cloud.firestore import Client, CollectionReference, Query
+from google.cloud.firestore import Client, CollectionReference, DELETE_FIELD, Query
 
 
 def prepare_client(project_id: str) -> Client:
@@ -6,28 +6,63 @@ def prepare_client(project_id: str) -> Client:
     return client
 
 
-def get_entries(client: Client, month: str) -> list[dict]:
-    collection_ref: CollectionReference = (client
+def get_collection(client: Client, month: str) -> CollectionReference:
+    return (client
         .collection('contents').document('diary')
         .collection('months').document(month)
-        .collection('entries')
+        .collection('entries'))
+
+
+def add_dummy_field(collection: CollectionReference) -> None:
+    for document in collection.stream():
+        collection.document(document.id).update({'dummy': 'dummy'})
+
+
+def remove_dummy_field(collection: CollectionReference) -> None:
+    for document in collection.stream():
+        collection.document(document.id).update({'dummy': DELETE_FIELD})
+
+
+def set_original_time(collection: CollectionReference) -> None:
+    for document in collection.stream():
+        collection.document(document.id).update({
+            'created_at': document.create_time,
+            'updated_at': document.update_time
+        })
+
+
+def set_date_as_id(collection: CollectionReference) -> None:
+    for document in collection.stream():
+        data = document.to_dict()
+        collection.document(document.id).delete()
+        new_document = collection.document(data['date'])
+        new_document.set(data)
+
+
+def set_id_as_date(collection: CollectionReference) -> None:
+    for document in collection.stream():
+        data = document.to_dict()
+        collection.document(document.id).delete()
+        new_document = collection.document(data['date'][8:])
+        new_document.set(data)
+
+
+def print_data(collection: CollectionReference) -> None:
+    query = (collection
         .order_by('date', direction=Query.DESCENDING)
-        .order_by('created_at', direction=Query.DESCENDING)
-    )
+        .order_by('created_at', direction=Query.DESCENDING))
 
-    entries = []
-    for entry in collection_ref.stream():
-        entries.append({'id': entry.id, **entry.to_dict()})
-
-    return entries
-
-
-def print_entries(entries: list[dict]) -> None:
-    for entry in entries:
-        print(entry)
+    for document in query.stream():
+        data = {'id': document.id, **document.to_dict()}
+        print(data)
 
 
 def main():
     client = prepare_client('ceshmina-shu-test')
-    entries = get_entries(client, '202305')
-    print_entries(entries)
+    collection = get_collection(client, '202305')
+    # add_dummy_field(collection)
+    # remove_dummy_field(collection)
+    set_date_as_id(collection)
+    set_id_as_date(collection)
+    set_original_time(collection)
+    print_data(collection)
